ed7fe54bc8da760d9f027aa8d586707776a0344c
silverwolf89 <silverwolf1987@hotmail.com>
2025-02-17 22:30:10 -0500

    creacion de las funcion agragar amigo

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//creamos la lista vacia donde se almacenará los nombres.
+let nombres=[];
+
+
+
+//creamos una funcion para que cada vez que se escriba un nombre se borre el espacio en la caja:
+
+function borrarCaja(){
+
+   return document.getElementById('amigo').value='';
+}
+
+
+
+
+
+
+//cuando escribimos el nombre en el espacio debemos capturar los valores 
+//creamos una funcion para esto:
+
+function agregarAmigo(){
+
+  let name = (document.getElementById('amigo').value);
+  console.log(name);
+  
+  
+  borrarCaja();
+
+//validar que no se envie un espacio vacio//
+  if (name ==='' || !isNaN(name) ){
+
+    alert('digita un nombre valido');
+
+  }else{
+    nombres.push(name);
+    console.log(nombres);
+  }
+}
+
+
+
